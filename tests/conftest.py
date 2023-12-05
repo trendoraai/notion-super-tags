df4d36ad87455a58015025bc269a7f437b7036e2
Pallav Bakshi <pallavbakshi@gmail.com>
2023-12-06 00:45:19 +0800

    feat: Get data from a given page_id

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+from dotenv import load_dotenv
+import sys
+import os
+import pytest
+from notion_client import Client
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
+load_dotenv()
+
+
+@pytest.fixture(scope="session")
+def notion():
+    return Client(auth=os.getenv("NOTION_TOKEN"))
+
+
+@pytest.fixture(scope="session")
+def page_id_to_test_basic_text():
+    return os.getenv("PAGE_ID")
+
+
+@pytest.fixture(scope="session")
+def page_id_to_test_mentions():
+    return os.getenv("PAGE_ID_FOR_MENTIONS")
